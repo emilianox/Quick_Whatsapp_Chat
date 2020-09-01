97dd0cd1292786f8651f1d446a8920ddd5a832b5
Emiliano Fernandez <emilianohfernandez@gmail.com>
2020-09-01 12:40:53 -0300

    add lock button

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -1,14 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {browser, Tabs} from 'webextension-polyfill-ts';
+import debounce from 'lodash.debounce';
 
 import './styles.scss';
 
 import 'react-phone-number-input/style.css';
 import PhoneInput from 'react-phone-number-input';
+import {LockFill} from 'react-bootstrap-icons';
 
 const Popup: React.FC = () => {
   const [numberToChat, setNumberToChat] = useState<string>('');
   const [prefixValue, setPrefixValue] = useState<string>('');
+  const [isLoading, setIsLoading] = useState(false);
   browser.storage.sync.get('prefixValue').then((result) => {
     setPrefixValue(result.prefixValue);
   });
@@ -18,15 +21,19 @@ const Popup: React.FC = () => {
   }
 
   const changePrefix = (valueFromWidget: string): void => {
-    // const {value} = event.target;
-    console.log('valueFromWidget', valueFromWidget);
     const value = valueFromWidget || '';
-    console.log('value', value);
-
-    browser.storage.sync.set({prefixValue: value}).then(() => {
-      setPrefixValue(value || '');
-    });
+    if (prefixValue !== value) {
+      setIsLoading(true);
+      browser.storage.sync.set({prefixValue: value}).then(() => {
+        setPrefixValue(value || '');
+      });
+    }
   };
+
+  useEffect(() => {
+    debounce(() => setIsLoading(false), 700)();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoading]);
 
   const handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void = (
     event
@@ -38,6 +45,7 @@ const Popup: React.FC = () => {
       window.close();
     });
   };
+
   return (
     <form
       onSubmit={handleSubmit}
@@ -48,29 +56,36 @@ const Popup: React.FC = () => {
           Quick Whatsapp Chat:
         </label>
       </div>
-      <div className="px-3 flex space-x-1">
-        <PhoneInput
-          id="Prefix"
-          name="numberToCall"
-          international
-          className="w-2/5"
-          value={prefixValue}
-          onChange={changePrefix}
-        />
-
-        <input
-          type="text"
-          id="numberToCall"
-          className="w-3/5"
-          name="numberToCall"
-          placeholder="Number"
-          spellCheck="false"
-          autoComplete="off"
-          /* eslint-disable jsx-a11y/no-autofocus */
-          autoFocus
-          value={numberToChat}
-          onChange={(e): void => setNumberToChat(e.target.value)}
-        />
+      <div className="flex px-3 space-x-1">
+        <div className="flex w-2/5">
+          <PhoneInput
+            id="Prefix"
+            name="numberToCall"
+            international
+            value={prefixValue}
+            className="w-10/12"
+            // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+            onChange={changePrefix}
+          />
+          <div className="btn-lock-div w-2/12">
+            {!isLoading && <LockFill className="wa-color" size={12} />}
+          </div>
+        </div>
+        <div className="w-3/5 ">
+          <input
+            type="text"
+            id="numberToCall"
+            className="w-full"
+            name="numberToCall"
+            placeholder="Number"
+            spellCheck="false"
+            autoComplete="off"
+            /* eslint-disable jsx-a11y/no-autofocus */
+            autoFocus
+            value={numberToChat}
+            onChange={(e): void => setNumberToChat(e.target.value)}
+          />
+        </div>
       </div>
       <div className="px-3 pb-2 flex flex-row-reverse">
         <input type="submit" className="btn btn-blue" value="Start chat" />
